9ac4408ff93cc26bfa195f91734f2a8caa76f988
tpereira <tpereira@student.42lisboa.com>
2021-10-26 20:07:31 +0100

    adding args to linkedlists

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 09:50:42 by tpereira          #+#    #+#             */
-/*   Updated: 2021/10/26 15:50:10 by tpereira         ###   ########.fr       */
+/*   Updated: 2021/10/26 19:52:56 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int	check_dup(int argc, char** argv)
 {
 	int	i;
 	int	j;
-	int	k;
 
 	i = 1;
 	while (argv[i])
@@ -24,7 +23,6 @@ int	check_dup(int argc, char** argv)
 		j = 2;
 		while (argv[j])
 		{
-			int test = ft_strcmp(argv[i], argv[j]);
 			if (i != j && ft_strcmp(argv[i], argv[j]) == 0)
 				return (0);
 			j++;
@@ -34,9 +32,35 @@ int	check_dup(int argc, char** argv)
 	return (1);
 }
 
+// void push(t_list * head, int val) {
+//     t_list * current = head;
+//     while (current->next != NULL) {
+//         current = current->next;
+//     }
+
+//     /* now we can add a new variable */
+//     current->next = (t_list *) malloc(sizeof(t_list));
+//     current->next->content = val;
+//     current->next->next = NULL;
+// }
+
 void	new_stacks(int argc, char** argv)
 {
-	write(1, "\n",1);
+	t_list	a;
+	// t_list	b;
+	// int		i;
+
+	// i = 2;
+	a = *ft_lstnew(argv[1]);
+	// while (argv[i])
+	// {
+	// 	ft_lstadd_front();
+	// 	push(a, ft_atoi(argv[i]));
+	// 	i++;
+	// }
+	// b = *ft_lstnew("");
+
+	write(1, (char* )a.content, ft_strlen((char *)a.content));
 }
 
 int	check_args(int argc, char** argv)
@@ -47,11 +71,18 @@ int	check_args(int argc, char** argv)
 		// write(1, &argv[i][0], ft_strlen(&argv[i][0]));
 		// write(1, " ", 1);
 		if (check_dup(argc, argv) == 0)
-			return write(1, "Duplicate\n", 10);
+		{
+			write(1, "Duplicate\n", 10);
+			return (0);
+		}
 		if (check_arg(argc, argv[i]) == 0)
-			return write(1, "Error\n", 6);
+		{
+			write(1, "Error\n", 6);
+			return (0);
+		}
 		i++;
 	}
+	return (1);
 		//write(1, "\n", 1);
 }
 
@@ -61,8 +92,10 @@ int main(int argc, char** argv)
 		write(1, "ERROR: not enough parameters", 28);
 	else if (argc >= 2)
 	{
-		check_args(argc, argv);
-		//new_stacks(argc, argv);
+		if (check_args(argc, argv) != 1);
+		{
+			new_stacks(argc, argv);
+		}
 	}
 	return (0);
 }

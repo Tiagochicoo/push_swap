7122dc4474f820592655a5388c13b4085fe1da54
tpereira <tpereira@student.42lisboa.com>
2022-01-13 22:25:42 +0000

    moving from A to B stack

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 09:50:42 by tpereira          #+#    #+#             */
-/*   Updated: 2022/01/13 22:10:26 by tpereira         ###   ########.fr       */
+/*   Updated: 2022/01/13 22:22:44 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,7 @@ void	pop_head(t_list**head)
 	temp = *head;
 	if (head == NULL || *head == NULL)
 		return;
-	
-	if ((*head)->next != NULL)
-		*head = (*head)->next;
+	*head = (*head)->next;
 
 	free(temp);
 }
@@ -43,7 +41,7 @@ t_list	*sort_stack(t_list**a, t_list**b)
 				pa(b, a);
 				pop_head(b);
 			}
-				pa(a, b);
+				pa(a, &b);
 		}
 		check_order(a);
 	}
